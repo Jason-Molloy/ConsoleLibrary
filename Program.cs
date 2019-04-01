be4ee6cbae1856bd51e1e23d71b87d3925f4b638
Jason Molloy <jmolloy619@gmail.com>
2019-03-31 20:17:09 -0600

    added books

--- Program.cs
+++ Program.cs
@@ -10,14 +10,24 @@ namespace console_library
       Book wtse = new Book("Where the Sidewalk Ends", "Shel Silverstien");
       Book lotr = new Book("Lord of the Rings", "JRR Tolkien");
       Library myLibrary = new Library("Jason's Library");
+      Book tbk = new Book("The Brothers Karamazov", "Fyodor Dostoyevsky");
+      Book fwtbt = new Book("For Whom the Bell Tolls", "Ernest Hemingway");
+      Book tspe = new Book("The Three Stigmata of Palmer Eldritch", "Philip K. Dick");
+      Book md = new Book("Moby Dick", "Herman Melville");
+      Book pf = new Book("Pale Fire", "Vladmir Nabokov");
+      Book gr = new Book("Gravity's Rainbow", "Thomas Pynchon");
 
       Console.Clear();
       Console.WriteLine($"Welcome to {myLibrary.Name}.");
-      //Console.WriteLine($"{lotr.Title} {lotr.Author} - {(lotr.Available ? "Available" : "Checked Out")}");
 
       myLibrary.AddBook(wtse);
       myLibrary.AddBook(lotr);
-
+      myLibrary.AddBook(tspe);
+      myLibrary.AddBook(fwtbt);
+      myLibrary.AddBook(tbk);
+      myLibrary.AddBook(pf);
+      myLibrary.AddBook(md);
+      myLibrary.AddBook(gr);
 
       myLibrary.PrintBooks();
     }
